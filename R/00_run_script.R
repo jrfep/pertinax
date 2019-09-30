f0760a7e388eb93f18f3fa2fc1884d8e39a2e4a6
Jose R FErrer Paris <jr.ferrer.paris@gmail.com>
2019-09-30 16:38:39 +1000

    Converting R script to markdown document

---
+++ R/00_run_script.R
@@ -0,0 +1,30 @@
+##R --vanilla
+library(nlme)
+##library(foreign)
+##library(lme4)
+##library(lattice)
+library(ape)
+
+if (Sys.getenv("WORKDIR") != "") {
+  ## en Linux/Mac podemos correr esto en el terminal:
+  ## source env/$(hostname).sh
+  work.dir <- Sys.getenv("WORKDIR")
+  script.dir <- Sys.getenv("SCRIPTDIR")
+} else {
+  ## si no hay que especificar las direcciones manualmente para
+  ## a) directorio de trabajo (puede ser un directorio temporal)
+  work.dir <- readline(prompt="Enter working directory: ")
+  ## b) dirección local del repositorio:
+  script.dir <- readline(prompt="Enter path to script directory: ")
+}
+
+system(sprintf("mkdir -p %s",work.dir))
+setwd(work.dir)
+
+## This will run the markdown document with R script to produce the output pdf
+
+mi.arch <- 'Analisis_S1_dur.Rmd'
+rmarkdown::render(sprintf("%s/R/%s",script.dir,mi.arch),"pdf_document")
+
+## preview document in linux
+##system(sprintf("atril %s/R/%s &",script.dir,gsub(".Rmd",".pdf",mi.arch)))
--- README.md
+++ README.md
@@ -4,3 +4,20 @@ Autores: Irene Zager, Jessica Eberhardt, Kathryn M. Rodríguez-Clark, and José 
 
 *![Aratinga pertinax2](assets/img/Apertinax_ilustracion_Papageien_small.png)*
 [Ilustración 29](https://biodiversitylibrary.org/page/40194378) en [Monographie der Papageien : oder, Vollständige Naturgeschichte aller bis jetzt bekannten Papageien mit getreuen und ausgemalten ...](https://doi.org/10.5962/bhl.title.61090) de [Christian Ludwig Brehm](https://de.wikipedia.org/wiki/Christian_Ludwig_Brehm)
+
+## Estructura del repositorio
+
+### env
+Archivos de configuración de entorno para usar en Linux/MAC OS.
+
+### data
+Conjuntos de datos para los análisis. Archivos de texto con valores separados por comas.
+
+### R
+Código en R para los análisis.
+
+### doc
+Documentos.
+
+### assets
+Archivos miscelaneos.
